fbddcabe721eea132cbbecc2d60cdd136d122e0f
Adilkhan <amedeuyev@gmail.com>
2021-05-15 13:01:43 +0600

    Initial Commit

---
+++ SwiftUITestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITestProject
+//
+//  Created by Адиль Медеуев on 15.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by Адиль Медеуев on 15.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
