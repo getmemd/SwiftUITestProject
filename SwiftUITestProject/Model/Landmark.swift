f3d90465de8a25e02b0f7f5a3bb4c0d32241dcbe
Adilkhan <amedeuyev@gmail.com>
2021-05-21 22:15:38 +0600

    Building lists and navigation

---
+++ SwiftUITestProject/Model/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  SwiftUITestProject
+//
+//  Created by Адиль Медеуев on 21.05.2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        .init(latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
