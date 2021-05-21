e7e9797632bb71398f7323f6eb3e361f7ce0d12a
Adilkhan <amedeuyev@gmail.com>
2021-05-21 08:31:17 +0600

    Initial commit

--- SwiftUITestProject/ContentView.swift
+++ SwiftUITestProject/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshu Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Description text goes here.")
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
